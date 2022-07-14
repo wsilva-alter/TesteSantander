a3f66ae5c7fe009c451764a0737acb6348a874c3
Wildson Santos <wildson.santos-external@gowithflow.io>
2022-07-14 23:06:25 +0100

    Initial Commit

---
+++ TesteSantander/TesteSantanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteSantanderApp.swift
+//  TesteSantander
+//
+//  Created by cliente on 14/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TesteSantanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
